5b43fcdc9607aa0d4f1548da70851c2327ccbfc0
TripKings <antonio.cicchese@gmail.com>
2020-01-30 21:09:14 -0600

    finished

--- src/App.js
+++ src/App.js
@@ -2,28 +2,10 @@ import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
 import Main from "./Components/Main";
-import ArcList from "./Components/ArcList"
+import Arc from "./Components/Arc"
 
 class App extends React.Component{
-  state = {
-    arcs: [],
-    input: ""
-  }
 
-  onChange = (e) => {
-    this.setState({input: e.target.value})
-
-  }
-
-  fetchArticles = (e) => {
-    e.preventDefault()
-    fetch("https://hn.algolia.com/api/v1/search?query=" + this.state.input)
-      .then(res => res.json())
-      .then(response => {
-        console.log(response)
-        this.setState({ arcs: response.hits})
-      });
-  };
 
 
 
@@ -31,15 +13,7 @@ class App extends React.Component{
     return (
       <div className="App">
         <header className="">
-          <form onSubmit={this.fetchArticles}>
-            <input onChange={this.onChange}></input>
-            <button type="submit">Submit</button>
-            {
-              this.state.arcs.map(arc => {
-                return <p>{arc.title}</p>
-              })
-            }
-          </form>
+          <Arc />
           
         </header>
       </div>
