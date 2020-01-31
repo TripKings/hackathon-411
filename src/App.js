7da0b393ff720b56bec24c88ae52e56ed7be864c
TripKings <antonio.cicchese@gmail.com>
2020-01-30 20:56:01 -0600

    just need to make a new componet

--- src/App.js
+++ src/App.js
@@ -2,14 +2,43 @@ import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
 import Main from "./Components/Main";
+import ArcList from "./Components/ArcList"
 
 class App extends React.Component{
+  state = {
+    arcs: [],
+    input: ""
+  }
+
+  onChange = (e) => {
+    this.setState({input: e.target.value})
+
+  }
+
+  fetchArticles = (e) => {
+    e.preventDefault()
+    fetch("https://hn.algolia.com/api/v1/search?query=" + this.state.input)
+      .then(res => res.json())
+      .then(response => {
+        console.log(response)
+        this.setState({ arcs: response.hits})
+      });
+  };
+
+
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <form>
-            <input label="Search"></input>
+        <header className="">
+          <form onSubmit={this.fetchArticles}>
+            <input onChange={this.onChange}></input>
+            <button type="submit">Submit</button>
+            {
+              this.state.arcs.map(arc => {
+                return <p>{arc.title}</p>
+              })
+            }
           </form>
           
         </header>
--- src/Components/Main.js
+++ src/Components/Main.js
@@ -4,13 +4,7 @@ class Main extends Component {
     articles: []
   };
 
-  fetchArticles = () => {
-    fetch("https://hn.algolia.com/api/v1/search?query=")
-      .then(res => res.json())
-      .then(response => {
-        this.setState({ articles: response.hits });
-      });
-  };
+  
   render() {
     console.log(this.state.articles);
     return (
